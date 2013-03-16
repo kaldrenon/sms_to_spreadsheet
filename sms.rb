73a88a5c1a18e707397c0a044197b8f5b62ac038
Andrew Fallows <kaldrenon@gmail.com>
2013-03-16 17:13:30 -0400

    added SendGrid code

--- sms.rb
+++ sms.rb
@@ -223,13 +223,14 @@ post '/email' do
       Pony.options = {
         :via => :smtp,
         :via_options => {
-        :address => 'smtp.sendgrid.net',
-        :port => '587',
-        :domain => 'heroku.com',
-        :user_name => ENV['SENDGRID_USERNAME'],
-        :password => ENV['SENDGRID_PASSWORD'],
-        :authentication => :plain,
-        :enable_starttls_auto => true
+          :address => 'smtp.sendgrid.net',
+          :port => '587',
+          :domain => 'heroku.com',
+          :user_name => ENV['SENDGRID_USERNAME'],
+          :password => ENV['SENDGRID_PASSWORD'],
+          :authentication => :plain,
+          :enable_starttls_auto => true
+        }
       }
 
       Pony.mail(
